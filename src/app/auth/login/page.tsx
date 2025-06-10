cef53a872cc7ea5878cbb5687bd02b279d238292
Yorukot <hi@yorukot.me>
2025-06-10 22:46:39 +0800

    feat: update package manager from npm to pnpm and add signup and login page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function LoginPage() {
+  return
+  (
+    <div>
+        <Button>Login</Button>
+      <h1>Login</h1>
+    </div>
+  );
+}
